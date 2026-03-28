6dc3771864ee56ba9cc8505f67acc8499e4723d9
daonq254 <dao.nq254@gmail.com>
2026-03-28 21:32:21 +0700

    3

--- Demo9_AccountManagement/hrm.js
+++ Demo9_AccountManagement/hrm.js
@@ -2,9 +2,11 @@ console.log("HRM");
 // Khai báo mảng global để lưu trữ danh sách tài khoản
 var listAccount = [];
 var listDepartment = [];
+var listPossition = [];
 // Load dữ liệu API Account
 getListAccount();
 getListDepartment();
+getListPosition();
 //
 function getListAccount(params) {
   $.ajax({
@@ -31,12 +33,32 @@ function getListDepartment(params) {
     // data: "data",
     dataType: "json",
     success: function (response) {
-      console.log("Response API Department: ", response);
+      // console.log("Response API Department: ", response);
       listDepartment = response;
       // Hiên thị dữ liệu ở dropdownlist Department
       for (let index = 0; index < listDepartment.length; index++) {
         $("#Department_ID").append(`
            <option value="${listDepartment[index].id}">${listDepartment[index].name}</option>
+          `);
+      }
+    },
+  });
+}
+
+function getListPosition(params) {
+  // Lấy dữ liệu API Position
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/api/v1/possitions",
+    // data: "data",
+    dataType: "json",
+    success: function (response) {
+      // console.log("Response API Position: ", response);
+      listPossition = response;
+      // Hiên thị dữ liệu ở dropdownlist Department
+      for (let index = 0; index < listPossition.length; index++) {
+        $("#Position_ID").append(`
+           <option value="${listPossition[index].id}">${listPossition[index].name}</option>
           `);
       }
     },
@@ -58,13 +80,13 @@ $("#reset_btn").click(function (e) {
 //   1. Xử lý bắt sự kiện khi người dùng nhấn nút Save
 $("#save_btn").click(function (e) {
   // 2. Lấy dữ liệu người dùng nhập
-  var v_ID_ID = $("#ID_ID").val();
+  // var v_ID_ID = $("#ID_ID").val();
   var v_Email_ID = $("#Email_ID").val();
   var v_Username_ID = $("#Username_ID").val();
   var v_Fullname_ID = $("#Fullname_ID").val();
   var v_Department_ID = $("#Department_ID").val();
   var v_Position_ID = $("#Position_ID").val();
-  var v_Cretate_Date_ID = $("#Cretate_Date_ID").val();
+  // var v_Cretate_Date_ID = $("#Cretate_Date_ID").val();
 
   // console.log("ID_ID: " + v_ID_ID);
   // console.log("Email_ID: " + v_Email_ID);
@@ -76,19 +98,38 @@ $("#save_btn").click(function (e) {
 
   // 3. Lưu trữ dữ liệu(FE)
   // Tạo đối tượng account
+  //   {
+  //     "email": "daonq123_test8",
+  //     "username": "daonq123_test8",
+  //     "fullname": "daonq123_test8",
+  //     "departmentId": "2",
+  //     "positionId": "2"
+  // }
+
   var account = {
-    Id: v_ID_ID,
-    Email: v_Email_ID,
-    Username: v_Username_ID,
-    Fullname: v_Fullname_ID,
-    Department: v_Department_ID,
-    Position: v_Position_ID,
-    Cretate_Date: v_Cretate_Date_ID,
+    // Id: v_ID_ID,
+    email: v_Email_ID,
+    username: v_Username_ID,
+    fullname: v_Fullname_ID,
+    departmentId: v_Department_ID,
+    positionId: v_Position_ID,
+    // Cretate_Date: v_Cretate_Date_ID,
   };
+
+  // Call API để lưu trữ dữ liệu account vào server Backend
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8080/api/v1/accounts",
+    data: JSON.stringify(account), // Chuyển đổi đối tượng account thành chuỗi JSON để gửi lên server
+    contentType: "application/json; charset=UTF-8",
+    success: function (response) {
+      getListAccount();
+    },
+  });
   // console.log(account);
 
   // Lưu trữ account vào mảng listAccount
-  listAccount.push(account);
+  // listAccount.push(account);
   //
 
   // 4. Hiển thị dữ liệu ở bảng kết quả
@@ -135,13 +176,27 @@ function handleDelete(indexParam) {
 function handleEdit(indexParam) {
   // console.log("Edit button indexParam: " + indexParam);
   var account_edit = listAccount[indexParam];
-  $("#ID_ID").val(account_edit.Id);
-  $("#Email_ID").val(account_edit.Email);
-  $("#Username_ID").val(account_edit.Username);
-  $("#Fullname_ID").val(account_edit.Fullname);
-  $("#Department_ID").val(account_edit.Department);
-  $("#Position_ID").val(account_edit.Position);
-  $("#Cretate_Date_ID").val(account_edit.Cretate_Date);
+  $("#ID_ID").val(account_edit.id);
+  $("#Email_ID").val(account_edit.email);
+  $("#Username_ID").val(account_edit.username);
+  $("#Fullname_ID").val(account_edit.fullname);
+  // account_edit.department tên phòng ban, nhưng dropdownlist Department_ID lại lưu trữ id của phòng ban, nên cần phải tìm id của phòng ban dựa vào tên phòng ban
+  // listDepartment
+  var v_DepId;
+  for (let index = 0; index < listDepartment.length; index++) {
+    if (listDepartment[index].name == account_edit.department) {
+      v_DepId = listDepartment[index].id;
+    }
+  }
+  $("#Department_ID").val(v_DepId);
+  var v_PosId;
+  for (let index = 0; index < listPossition.length; index++) {
+    if (listPossition[index].name == account_edit.position) {
+      v_PosId = listPossition[index].id;
+    }
+  }
+  $("#Position_ID").val(v_PosId);
+  $("#Cretate_Date_ID").val(account_edit.createDate);
 
   // var indexAccountEdit = indexParam;
 }
@@ -156,7 +211,27 @@ $("#update_btn").click(function (e) {
   var v_Department_ID_Update = $("#Department_ID").val();
   var v_Position_ID_Update = $("#Position_ID").val();
   var v_Cretate_Date_ID_Update = $("#Cretate_Date_ID").val();
-
+  //
+  // {
+  //   "fullname": "DAONQ123",
+  //   "departmentId": "2",
+  //   "positionId": "2"
+  // }
+  var account_update = {
+    fullname: v_Fullname_ID_Update,
+    departmentId: v_Department_ID_Update,
+    positionId: v_Position_ID_Update,
+  };
+  // Update API để lưu trữ dữ liệu account vào server Backend
+  $.ajax({
+    type: "PUT",
+    url: "http://localhost:8080/api/v1/accounts/" + v_ID_ID,
+    data: JSON.stringify(account_update), // Chuyển đổi đối tượng account thành chuỗi JSON để gửi lên server
+    contentType: "application/json; charset=UTF-8",
+    success: function (response) {
+      getListAccount();
+    },
+  });
   // console.log("ID_ID: " + v_ID_ID);
   // console.log("Email_ID: " + v_Email_ID);
   // console.log("Username_ID: " + v_Username_ID);
@@ -166,19 +241,19 @@ $("#update_btn").click(function (e) {
   // console.log("Cretate_Date_ID: " + v_Cretate_Date_ID);
   // listAccount
   // Tìm Account mà đang cần Update
-  var indexAccountEdit;
-  for (let index = 0; index < listAccount.length; index++) {
-    if (listAccount[index].Id == v_ID_ID) {
-      indexAccountEdit = index;
-    }
-  }
-  // Cập nhật lại thông tin cho Account cần Update
-  listAccount[indexAccountEdit].Email = v_Email_ID_Update;
-  listAccount[indexAccountEdit].Username = v_Username_ID_Update;
-  listAccount[indexAccountEdit].Fullname = v_Fullname_ID_Update;
-  listAccount[indexAccountEdit].Department = v_Department_ID_Update;
-  listAccount[indexAccountEdit].Position = v_Position_ID_Update;
-  listAccount[indexAccountEdit].Cretate_Date = v_Cretate_Date_ID_Update;
+  // var indexAccountEdit;
+  // for (let index = 0; index < listAccount.length; index++) {
+  //   if (listAccount[index].Id == v_ID_ID) {
+  //     indexAccountEdit = index;
+  //   }
+  // }
+  // // Cập nhật lại thông tin cho Account cần Update
+  // listAccount[indexAccountEdit].Email = v_Email_ID_Update;
+  // listAccount[indexAccountEdit].Username = v_Username_ID_Update;
+  // listAccount[indexAccountEdit].Fullname = v_Fullname_ID_Update;
+  // listAccount[indexAccountEdit].Department = v_Department_ID_Update;
+  // listAccount[indexAccountEdit].Position = v_Position_ID_Update;
+  // listAccount[indexAccountEdit].Cretate_Date = v_Cretate_Date_ID_Update;
 
-  showListAccount();
+  // showListAccount();
 });
