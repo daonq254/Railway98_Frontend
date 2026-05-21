83fb19da162930e1b5695f503360dfd2424f88e2
daonq254 <dao.nq254@gmail.com>
2026-05-21 21:27:14 +0700

    3

--- REACTJS_R98/2. ReactJS/LAB5/demorouter/src/App.js
+++ REACTJS_R98/2. ReactJS/LAB5/demorouter/src/App.js
@@ -1,26 +1,48 @@
 import './App.css';
+import { Route, Routes, Link } from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
+import AccountManagement from './Components/AccountManagement';
+import DepartmentManagement from './Components/DepartmentManagement';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+import Error from './Components/Error';
 
 function App() {
   return (
     <div class="container">
+      {/* Menu */}
       <div className="row">
         <nav className="navbar navbar-inverse" style={{ backgroundColor: "black", color: "black" }}>
           <div className="container-fluid">
             <ul class="nav navbar-nav">
-              <li><a href="#">VTI ACADEMY</a></li>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">AccountManagement</a></li>
-              <li><a href="#">DepartmentManagement</a></li>
+              <li><Link to="/home">VTI ACADEMY</Link></li>
+              <li><Link to="/home">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/AccountManagement">AccountManagement</Link></li>
+              <li><Link to="/DepartmentManagement">DepartmentManagement</Link></li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
-              <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
-              <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+              <li><Link to="/Sign_Up"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
+              <li><Link to="/Login"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
             </ul>
           </div>
         </nav>
       </div>
 
+      {/* Content */}
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path="/AccountManagement" element={<AccountManagement />} />
+        <Route path="/DepartmentManagement" element={<DepartmentManagement />}
+        />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Sign_Up" element={<SignUp />} />
+        <Route path="*" element={<Error />} />
+
+      </Routes>
+      {/*  */}
     </div>
   );
 }
